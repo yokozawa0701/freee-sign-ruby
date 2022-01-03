cb44b75f6a3f7ffc7cbc9b41b15c1f67ab9f85dc
yokozawa0701 <d.yoko.0701@gmail.com>
2022-01-03 09:05:20 +0900

    initial commit

---
+++ lib/ninjasign/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Ninjasign
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/ninjasign/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ninjasign::Ruby do
+  it "has a version number" do
+    expect(Ninjasign::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
