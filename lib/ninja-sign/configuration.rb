dd438763b414030744e7bf1175bc5de2b4defe26
yokozawa0701 <d.yoko.0701@gmail.com>
2022-01-23 21:55:42 +0900

    環境変数の値を読み込むように

--- lib/ninja-sign/configuration.rb
+++ lib/ninja-sign/configuration.rb
@@ -5,5 +5,15 @@ module NinjaSign
     VALID_OPTIONS_KEYS = %i[endpoint client_id client_secret].freeze
 
     attr_accessor(*VALID_OPTIONS_KEYS)
+
+    def self.extended(base)
+      base.reset
+    end
+
+    def reset
+      self.endpoint = ENV["ENDPOINT"]
+      self.client_id = ENV["CLIENT_ID"]
+      self.client_secret = ENV["CLIENT_SECRET"]
+    end
   end
 end
