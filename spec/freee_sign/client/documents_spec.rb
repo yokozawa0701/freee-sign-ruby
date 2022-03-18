cb5a36098a770cbefce0dd2b38babd61fe093df0
noriyotcp <noriyo.akita@gmail.com>
2022-03-18 19:02:00 +0900

    Fix rubocop warnings

--- spec/freee_sign/client/documents_spec.rb
+++ spec/freee_sign/client/documents_spec.rb
@@ -1,32 +1,32 @@
 # frozen_string_literal: true
 
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe FreeeSign::Client::Documents do
   let(:client) { FreeeSign::Client.new }
 
-  describe "#documents" do
+  describe '#documents' do
     subject { client.documents(params) }
 
-    context "with no params" do
+    context 'with no params' do
       let(:params) { {} }
 
-      before { stub_get("/v1/documents", "documents") }
+      before { stub_get('/v1/documents', 'documents') }
 
       it do
         expect(subject.size).to eq 2
-        expect(a_get("/v1/documents")).to have_been_made.once
+        expect(a_get('/v1/documents')).to have_been_made.once
       end
     end
 
-    context "with :per_page" do
+    context 'with :per_page' do
       let(:params) { { per_page: 1 } }
 
-      before { stub_get("/v1/documents", "documents").with(query: params) }
+      before { stub_get('/v1/documents', 'documents').with(query: params) }
 
       it do
         subject
-        expect(a_get("/v1/documents").with(query: params)).to have_been_made.once
+        expect(a_get('/v1/documents').with(query: params)).to have_been_made.once
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,13 +27,13 @@ def load_fixture(name)
 end
 
 # GET
-def stub_get(path, fixture, status_code=200)
-  stub_request(:get, "#{FreeeSign::ENDPOINT}#{path}").
-    with(:headers => {'Authorization' => "Bearer #{FreeeSign.access_token}", 'Content-Type' => 'application/json'}).
-    to_return(body: load_fixture(fixture), status: status_code)
+def stub_get(path, fixture, status_code = 200)
+  stub_request(:get, "#{FreeeSign::ENDPOINT}#{path}")
+    .with(headers: { 'Authorization' => "Bearer #{FreeeSign.access_token}", 'Content-Type' => 'application/json' })
+    .to_return(body: load_fixture(fixture), status: status_code)
 end
 
 def a_get(path)
-  a_request(:get, "#{FreeeSign::ENDPOINT}#{path}").
-    with(:headers => {'Authorization' => "Bearer #{FreeeSign.access_token}", 'Content-Type' => 'application/json'})
+  a_request(:get, "#{FreeeSign::ENDPOINT}#{path}")
+    .with(headers: { 'Authorization' => "Bearer #{FreeeSign.access_token}", 'Content-Type' => 'application/json' })
 end
