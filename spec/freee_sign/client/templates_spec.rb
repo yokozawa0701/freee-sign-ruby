dee6d37eaa534d53130328bd9d0a3632388ab662
yokozawa0701 <d.yoko.0701@gmail.com>
2022-04-01 18:55:55 +0900

    /v1/templatesのテスト追加

---
+++ spec/freee_sign/client/templates_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe FreeeSign::Client::Templates do
+  let(:client) { FreeeSign::Client.new }
+
+  describe '#templates' do
+    subject { client.templates(params) }
+
+    before do
+      allow_any_instance_of(FreeeSign::Client).to receive(:access_token).and_return('access_token')
+    end
+
+    context 'with no params' do
+      let(:params) { {} }
+
+      before { stub_get('/v1/templates', 'templates') }
+
+      it do
+        expect(subject.size).to eq 2
+        expect(a_get('/v1/templates')).to have_been_made.once
+      end
+    end
+
+    context 'with :per_page' do
+      let(:params) { { per_page: 1 } }
+
+      before { stub_get('/v1/templates', 'templates').with(query: params) }
+
+      it do
+        subject
+        expect(a_get('/v1/templates').with(query: params)).to have_been_made.once
+      end
+    end
+  end
+end
