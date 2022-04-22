87785b77ed612f65af119cd09fa9a697b24ecb44
yokozawa0701 <d.yoko.0701@gmail.com>
2022-04-22 18:53:12 +0900

    Add /v1/users test with params

--- spec/freee_sign/client/users_spec.rb
+++ spec/freee_sign/client/users_spec.rb
@@ -5,18 +5,33 @@ require 'spec_helper'
 RSpec.describe FreeeSign::Client::Users do
   let(:client) { FreeeSign::Client.new }
 
-  describe '#users' do
-    subject { client.team }
+  before do
+    allow_any_instance_of(FreeeSign::Client).to receive(:access_token).and_return('access_token')
+  end
 
-    before do
-      allow_any_instance_of(FreeeSign::Client).to receive(:access_token).and_return('access_token')
+  describe '#users' do
+    subject { client.users(params) }
+
+    context 'with no params' do
+      let(:params) { {} }
+
+      before { stub_get('/v1/users', 'users') }
+
+      it do
+        expect(subject.size).to eq 2
+        expect(a_get('/v1/users')).to have_been_made.once
+      end
     end
 
-    before { stub_get('/v1/users', 'users') }
+    context 'with :per_page' do
+      let(:params) { { per_page: 1 } }
 
-    it do
-      subject
-      expect(a_get('/v1/users')).to have_been_made.once
+      before { stub_get('/v1/users', 'users').with(query: params) }
+
+      it do
+        subject
+        expect(a_get('/v1/users').with(query: params)).to have_been_made.once
+      end
     end
   end
 end
